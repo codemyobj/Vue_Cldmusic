e938c6885a463a40be6d7970f9453e6c7949ce69
codemyobj <459324704@qq.com>
2021-08-10 23:46:31 +0800

    初始化项目目录结构

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import mutations from "./mutations";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state = {};
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state,
+  mutations,
+});
