05816309ccfd01753af40c7f7744fd4b7fd1d7f4
codemyobj <459324704@qq.com>
2021-10-30 12:50:14 +0800

    下载项目依赖插件配置

---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header> </el-header>
+  </el-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {},
+  name: "Index",
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped></style>
